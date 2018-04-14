c836730299004a4158d74e4d7171973211c14c8e
A Hoff <ahoff@Andrews-MBP.hitronhub.home>
2018-04-13 21:05:01 -0500

    init

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	appengine.Main()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
